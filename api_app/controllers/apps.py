e258ba24f76a633236ad222051e50f0c4ec1b155
james <james@bubbleye.com>
2023-10-19 00:29:59 +0800

    Working version of /apps/ api paths, no react app yet

--- api_app/controllers/apps.py
+++ api_app/controllers/apps.py
@@ -3,8 +3,13 @@ import os
 
 from litestar import Controller, get
 
-from api_app.models import AppDetail, AppOverview
-from config.config import IMAGES_DIR
+from api_app.models import AppDetail, AppsOverview
+
+from dbcon.queries import get_appstore_categories, query_recent_apps, get_single_app
+
+from config import get_logger
+
+logger = get_logger(__name__)
 
 """
 /apps/{store_id} a specific app
@@ -17,41 +22,68 @@ def get_string_date_from_days_ago(days: int) -> str:
     return mydate_str
 
 
-def get_most_recent_storm_dirs() -> tuple[str, list[str]]:
-    storm_dirs: list = []
-    i = 0
-    while len(storm_dirs) == 0 and i <= 5:
-        date_str = get_string_date_from_days_ago(i)
-        if date_str in os.listdir(IMAGES_DIR):
-            storm_dirs = os.listdir(f"{IMAGES_DIR}/{date_str}")
-            print(f"{date_str=} found {len(storm_dirs)} directories")
-        else:
-            print(f"{date_str=} found no directories")
-        i += 1
-    return date_str, storm_dirs
+def category_overview() -> dict:
+    cats = get_appstore_categories()
+    # Make app count strings
+    cats["android"] = cats["android"].apply(
+        lambda x: "{:,.0f}".format(x) if x else "N/A"
+    )
+    cats["ios"] = cats["ios"].apply(lambda x: "{:,.0f}".format(x) if x else "N/A")
+    category_dicts = cats.to_dict(orient="records")
+    return category_dicts
+
+
+def get_app_overview_dict() -> AppsOverview:
+    new_apps = query_recent_apps(days=7)
+    trending_apps = query_recent_apps(days=30)
+    trending_ios_apps = trending_apps[~trending_apps["store"].str.contains("oogl")]
+    trending_google_apps = trending_apps[trending_apps["store"].str.contains("oogl")]
+    new_ios_dicts = new_apps[~new_apps["store"].str.contains("oogl")].to_dict(
+        orient="records"
+    )
+    new_google_dicts = new_apps[new_apps["store"].str.contains("oogl")].to_dict(
+        orient="records"
+    )
+    trending_google_dicts = trending_google_apps.to_dict(orient="records")
+    trending_ios_dicts = trending_ios_apps.to_dict(orient="records")
+    trending_title = "Trending Apps this Month"
+    recent_title = "New Apps this Month"
+    trending_dicts: dict = {}
+    trending_dicts[trending_title] = {}
+    trending_dicts[recent_title] = {}
+    trending_dicts["new_google"] = new_google_dicts
+    trending_dicts["new_ios"] = new_ios_dicts
+    trending_dicts["trending_google"] = trending_google_dicts
+    trending_dicts["trending_ios"] = trending_ios_dicts
+    my_dict = AppsOverview(
+        new_google=new_google_dicts,
+        new_ios=new_ios_dicts,
+        trending_google=trending_google_dicts,
+        trending_ios=trending_ios_dicts,
+    )
+    return my_dict
 
 
 class AppController(Controller):
     path = "/api/apps"
 
     @get(path="/")
-    async def get_apps_overview(self) -> AppOverview:
+    async def get_apps_overview(self) -> AppsOverview:
         """
         Handles a GET request for a list of apps
 
         Args:
 
         Returns:
-            Storms: A dictionary representation of the list of apps.
+            A dictionary representation of the list of apps for homepasge
         """
-        date_str, storm_dirs = get_most_recent_storm_dirs()
+        logger.info("inside a request")
+        home_dict = get_app_overview_dict()
 
-        mydict = Storms([Storm(id=mystorm, date=date_str) for mystorm in storm_dirs])
-
-        return mydict
+        return home_dict
 
     @get(path="/{store_id:str}", cache=3600)
-    async def get_storm_image(self, store_id: str) -> AppDetail:
+    async def get_app_detail(self, store_id: str) -> AppDetail:
         """
         Handles a GET request for a specific app.
 
@@ -62,5 +94,7 @@ class AppController(Controller):
             json
         """
 
-        app_dict = {"store_id"}
+        app_df = get_single_app(store_id)
+        app_dict = app_df.to_dict(orient="records")[0]
+
         return app_dict
--- app.py
+++ app.py
@@ -1,26 +1,35 @@
 from litestar import Litestar
-from litestar.logging import LoggingConfig
+
+# from litestar.logging import LoggingConfig
 from litestar.openapi import OpenAPIConfig, OpenAPIController
 
+from litestar.logging import LoggingConfig
+
 from api_app.controllers.apps import AppController
+
+from config import get_logger
+
+logger = get_logger(__name__)
 
 
 class MyOpenAPIController(OpenAPIController):
     path = "/api/docs"
 
 
+logging_config = LoggingConfig(
+    loggers={
+        "my_app": {
+            "level": "INFO",
+            "handlers": ["queue_listener"],
+        }
+    }
+)
+
 app = Litestar(
     route_handlers=[AppController],
     openapi_config=OpenAPIConfig(
-        title="HackerNews API", version="1.0.0", openapi_controller=MyOpenAPIController
+        title="App Store API", version="0.0.1", openapi_controller=MyOpenAPIController
     ),
-    logging_config=LoggingConfig(
-        loggers={
-            "my_app": {
-                "level": "INFO",
-                "handlers": ["queue_listener"],
-            }
-        }
-    ),
+    logging_config=logging_config,
     debug=True,
 )
