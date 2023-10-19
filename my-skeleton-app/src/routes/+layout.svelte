bb4c4cab4777cbd0cc659f60ba8cf2d3a31a0f54
james <james@bubbleye.com>
2023-10-20 02:46:13 +0800

    Working version of skeleton and existing pages

--- my-skeleton-app/src/routes/+layout.svelte
+++ my-skeleton-app/src/routes/+layout.svelte
@@ -12,27 +12,12 @@
 				<strong class="text-xl uppercase">App Store Data</strong>
 			</svelte:fragment>
 			<a href="/" class="btn variant-filled">Home</a>
-			<a href="/" class="btn variant-filled">About</a>
+			<a href="https://jamesoclaire.com" class="btn variant-filled">Blog</a>
+			<a href="/about" class="btn variant-filled">About</a>
 			<svelte:fragment slot="trail">
 				<a
 					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
+					href="https://github.com/ddxv/app-store-dash"
 					target="_blank"
 					rel="noreferrer"
 				>
@@ -43,5 +28,6 @@
 	</svelte:fragment>
 
 	<!-- Page Route Content -->
+
 	<slot />
 </AppShell>
--- my-skeleton-app/src/routes/+page.svelte
+++ my-skeleton-app/src/routes/+page.svelte
@@ -7,7 +7,6 @@
 	import Rating from './Rating.svelte';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
 <AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
 	<svelte:fragment slot="sidebarLeft">
 		<!-- Insert the list: -->
@@ -29,36 +28,41 @@
 
 		<!-- --- -->
 	</svelte:fragment>
-</AppShell>
+	<h1>
+		Welcome to My Apps Dash. Feel free to look around, if you have any questions just reach out or
+		ping me on GitHub.
+	</h1>
+	<p>
+		Visit <a href="https://github.com/ddxv/app-store-dash">github.com/ddxv/app-store-dash</a> to read
+		the documentation
+	</p>
 
-<h1>Welcome to SvelteKitxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<div>
-	{#if data}
-		{#each Object.entries(data) as [title, collection]}
-			<h2>{title}</h2>
-			{#each Object.entries(collection) as [appstore, apps]}
-				<h3>{appstore}</h3>
-				<div class="app-grid">
-					{#each apps as app}
-						<div class="app-item">
-							<a href={`/apps/${app.store_id}`} style="text-decoration: none; color: inherit;">
-								<img src={app.icon_url_512} alt={app.name} class="app-icon" />
-								<div>{app.name}</div>
-								<Rating total={5} size={20} rating={app.rating} />
-							</a>
-						</div>
-					{/each}
-				</div>
-				<hr class="section-divider" />
+	<div>
+		{#if data}
+			{#each Object.entries(data) as [title, collection]}
+				<h1>{title}</h1>
+				{#each Object.entries(collection) as [appstore, apps]}
+					<h2>{appstore}</h2>
+					<div class="app-grid">
+						{#each apps as app}
+							<div class="app-item">
+								<a href={`/apps/${app.store_id}`} style="text-decoration: none; color: inherit;">
+									<img src={app.icon_url_512} alt={app.name} class="app-icon" />
+									<div>{app.name}</div>
+									<Rating total={5} size={20} rating={app.rating} />
+								</a>
+							</div>
+						{/each}
+					</div>
+					<hr class="section-divider" />
+				{/each}
 			{/each}
-		{/each}
-	{:else}
-		<p>Loading...</p>
-		{data}
-	{/if}
-</div>
+		{:else}
+			<p>Loading...</p>
+			{data}
+		{/if}
+	</div>
+</AppShell>
 
 <style>
 	/* Container Grid */
