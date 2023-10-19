08a87b245a4ee1601c5c9bed36dac0c299c870d9
james <james@bubbleye.com>
2023-10-20 02:29:37 +0800

    Merging in existing changes for displaying apps

---
+++ my-skeleton-app/src/routes/apps/[id]/+page.server.js
@@ -0,0 +1,29 @@
+export const ssr = true;
+export const csr = true;
+console.log('Script executed');
+
+/** @type {import('../[id]/$types').PageServerLoad} */
+export async function load({ params }) {
+	console.log('load app started'); try {
+		const id = params.id;
+
+		const res = await fetch(`http://localhost:8000/api/apps/${id}`);
+
+		if (!res.ok) {
+			throw new Error(`Failed to fetch ${id} with status ${res.status}`);
+		}
+
+		const app_detail = await res.json();
+		console.log(`loaded app_detail with len: ${Object.keys(app_detail).length}`);
+		return { myapp: app_detail };
+
+	} catch (error) {
+		console.error('Failed to load data:', error);
+		return {
+			status: 500,
+			error: 'Failed to load trending apps'
+		};
+	}
+}
+
+
