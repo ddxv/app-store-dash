02f5b197e190191131d0de0adc42e10f77c2ed17
james <james@bubbleye.com>
2023-10-24 12:03:06 +0800

    Working collections navigation, broken home page

---
+++ frontend/src/routes/collections/[collection]/+page.server.ts
@@ -0,0 +1,34 @@
+export const ssr: boolean = true;
+export const csr: boolean = true;
+
+console.log('Script executed');
+
+interface LoadResponse {
+    myapps: any;
+    status?: number;
+    error?: string;
+}
+/** @type {import('../[collection]/$types').PageServerLoad} */
+export async function load({ params }): Promise<LoadResponse> {
+    const collectionValue = params.collection
+    console.log(`load started collection=${collectionValue}`);
+    try {
+        const res = await fetch(`http://localhost:8000/api/apps/collections/${collectionValue}`);
+
+        if (!res.ok) {
+            const text = await res.text();
+            throw new Error(`Failed to fetch collections status ${res.status} ${text}`);
+        }
+
+        const app_collections: any = await res.json();
+        console.log(`loaded collections with len: ${Object.keys(app_collections).length}`);
+        return { myapps: app_collections };
+    } catch (error) {
+        console.error('Failed to load data:', error);
+        return {
+            myapps: {},
+            status: 500,
+            error: 'Failed to load trending apps'
+        };
+    }
+}
---
+++ frontend/src/routes/index.svelte
@@ -0,0 +1,7 @@
+<script context="module">
+	export async function load() {
+		return {
+			redirect: '/collections/new_weekly'
+		};
+	}
+</script>
