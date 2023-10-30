e0e84dbb82a296df67d688782b282d9a79af5302
james <james@bubbleye.com>
2023-10-30 16:27:56 +0800

    Streaming promise for app details page and search

--- frontend/src/routes/apps/[id]/+page.server.ts
+++ frontend/src/routes/apps/[id]/+page.server.ts
@@ -8,35 +8,23 @@ export const load: PageServerLoad = async ({ params, locals }) => {
 	try {
 		const id = params.id;
 
-		const res = await fetch(`http://localhost:8000/api/apps/${id}`);
+		const res = fetch(`http://localhost:8000/api/apps/${id}`);
 
-		if (!res.ok) {
-			throw new Error(`Failed to fetch app ${id} with status ${res.status}`);
-		}
-
-		const app_detail = await res.json();
-		console.log(`loaded app_detail with len: ${Object.keys(app_detail).length}`);
-
-		let rank_details = {};
-
-		try {
-			const ranks = await fetch(`http://localhost:8000/api/apps/${id}/ranks`);
-			if (!ranks.ok) {
-				console.warn(`Failed to fetch ranks for app ${id} with status ${ranks.status}`);
-			} else {
-				rank_details = await ranks.json();
-			}
-		} catch (rankError) {
-			console.warn('Failed to fetch ranks:', rankError);
-		}
+		const ranks = fetch(`http://localhost:8000/api/apps/${id}/ranks`);
 
 		return {
-			myapp: app_detail,
-			myranks: rank_details
+			myapp: {
+				streamed: res.then((resp) => resp.json())
+			},
+			myranks: {
+				streamed: ranks.then((resp) => resp.json())
+			}
 		};
 	} catch (error) {
 		console.error('Failed to load app data:', error);
 		return {
+			myapp: { streamed: {} },
+			myranks: { streamed: {} },
 			status: 500,
 			error: 'Failed to load trending apps'
 		};
--- frontend/src/routes/search/[term]/+page.svelte
+++ frontend/src/routes/search/[term]/+page.svelte
@@ -3,10 +3,13 @@
 
 	import AppGroupCard from '$lib/AppGroupCard.svelte';
 
-	/** @type {import('../[term]/$types').PageData} */
 	export let data: SearchResponse;
 </script>
 
-{#if data.results}
-	<AppGroupCard apps={data.results} />
-{/if}
+{#await data.results.streamed}
+	Loading ...
+{:then results}
+	<AppGroupCard apps={results} />
+{:catch}
+	Loading ...
+{/await}
