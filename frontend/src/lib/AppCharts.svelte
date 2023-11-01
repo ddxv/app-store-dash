7187011b4b405d508df90b916fb61393bf3809e8
james <james@bubbleye.com>
2023-11-01 12:33:34 +0800

    Working versions of both chart.js and echarts

--- frontend/src/lib/AppCharts.svelte
+++ frontend/src/lib/AppCharts.svelte
@@ -2,130 +2,10 @@
 	import { onMount } from 'svelte';
 	import Chart from 'chart.js/auto';
 
-	let data = [20, 100, 50, 12, 20, 130, 45];
-	let labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-	// const mylabels = Utils.months({ count: 7 });
-	// const mydata = {
-	// 	labels: labels,
-	// 	datasets: [
-	// 		{
-	// 			label: 'My First Dataset',
-	// 			data: [65, 59, 80, 81, 56, 55, 40],
-	// 			fill: false,
-	// 			borderColor: 'rgb(75, 192, 192)',
-	// 			tension: 0.1
-	// 		}
-	// 	]
-	// };
-
 	//WORKING FOR EXAMPLE LINE
 	let ctx;
 	let canvas;
-	let myRed = 'rgb(255, 99, 132)';
-	let myoptions = {
-		options: {
-			interaction: {
-				mode: 'nearest',
-				intersect: true
-			},
-			events: ['mousemove', 'mouseout', 'click', 'touchstart', 'touchmove']
-		},
-		type: 'line',
-		data: {
-			labels: labels,
-			datasets: [
-				{
-					label: 'Unit Sales',
-					data: data,
-					borderWidth: 5,
-					hoverBackgroundColor: myRed,
-					hoverBorderJoinStyle: 'round',
-					tension: 0.3,
-					// fill: true,
-					pointBackgroundColor: myRed,
-					backgroundColor: myRed,
-					borderColor: myRed,
-					pointStyle: 'circle',
-					pointRadius: 10,
-					pointHoverRadius: 15
-				}
-			]
-		}
-	};
-
-	var OGoptions = {
-		type: 'line',
-		data: {
-			labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-			datasets: [
-				{
-					label: '# of Votes',
-					data: [12, 19, 3, 5, 2, 3],
-					backgroundColor: '#FF0000',
-					borderWidth: 5,
-					borderColor: '#FF0000'
-				},
-				{
-					label: '# of Points',
-					data: [7, 11, 5, 8, 3, 7],
-					backgroundColor: '#0000FF',
-					borderWidth: 5,
-					borderColor: '#0000FF'
-				},
-				{
-					label: '# of Cars',
-					data: [18, 4, 12, 6, 9, 2],
-					backgroundColor: '#FF1493',
-					borderWidth: 5,
-					borderColor: '#FF1493'
-				}
-			]
-		},
-		options: {
-			onHover: (e, activeEls, chart) => {
-				if (activeEls.length === 0) {
-					chart.data.datasets.forEach((dataset) => {
-						dataset.borderWidth = dataset.borderWidth == 15 ? 5 : dataset.borderWidth;
-						dataset.backgroundColor =
-							dataset.backgroundColor.length === 9
-								? dataset.backgroundColor.slice(0, -2)
-								: dataset.backgroundColor;
-						dataset.borderColor =
-							dataset.borderColor.length === 9
-								? dataset.borderColor.slice(0, -2)
-								: dataset.borderColor;
-					});
-					chart.update();
-					return;
-				}
-				const hoveredEl = chart.getElementsAtEventForMode(
-					e,
-					'point',
-					{
-						intersect: false,
-						axis: 'x'
-					},
-					true
-				)[0];
-				chart.data.datasets.forEach((dataset, i) => {
-					dataset.borderWidth =
-						hoveredEl.datasetIndex === i || dataset.borderWidth.length == 5
-							? 15
-							: dataset.borderWidth;
-					dataset.backgroundColor =
-						hoveredEl.datasetIndex === i || dataset.backgroundColor.length === 9
-							? dataset.backgroundColor
-							: dataset.backgroundColor + '4D';
-					dataset.borderColor =
-						hoveredEl.datasetIndex === i || dataset.borderColor.length === 9
-							? dataset.borderColor
-							: dataset.borderColor + '4D';
-				});
-				chart.update();
-			}
-		}
-	};
+	const myDraculaColors = ['#ff79c6', '#bd93f9', '#1ef956', '#f67516'];
 
 	const hoverlineoptions = {
 		type: 'line',
@@ -134,69 +14,65 @@
 			datasets: [
 				{
 					label: '# of Votes',
-					data: [12, 19, 3, 5, 2, 3],
-					backgroundColor: '#FF0000',
+					yAxisID: 'y1',
+					data: [1, 1, 2, 3, 6, 8],
+					backgroundColor: myDraculaColors[0],
 					borderWidth: 4,
-					borderColor: '#FF0000'
+					borderColor: myDraculaColors[0],
+					tension: 0.5,
+					pointRadius: 5,
+					pointHitRadius: 5
 				},
 				{
 					label: '# of Points',
-					data: [7, 11, 5, 8, 3, 7],
-					backgroundColor: '#0000FF',
+					yAxisID: 'y1',
+					data: [2, 4, 4, 5, 3, 5],
+					backgroundColor: myDraculaColors[1],
 					borderWidth: 4,
-					borderColor: '#0000FF'
+					borderColor: myDraculaColors[1],
+					tension: 0.5,
+					pointRadius: 5,
+					pointHitRadius: 5
 				},
 				{
 					label: '# of Cars',
-					data: [18, 4, 12, 6, 9, 2],
-					backgroundColor: '#FF1493',
+					yAxisID: 'y1',
+					data: [5, 6, 7, 6, 5, 4],
+					backgroundColor: myDraculaColors[2],
 					borderWidth: 4,
-					borderColor: '#FF1493'
+					borderColor: myDraculaColors[2],
+					tension: 0.5,
+					pointRadius: 5,
+					pointHitRadius: 5
 				}
 			]
 		},
 		options: {
+			scales: { y1: { reverse: true, type: 'linear', display: true, position: 'left' } },
 			responsive: true,
 			interaction: {
 				intersect: false,
 				mode: 'nearest'
 			},
-			events: ['mousemove', 'mouseout', 'click', 'touchstart', 'touchmove'],
-			onLeave: (event, chart) => {
+			// events: ['mousemove', 'mouseout', 'click', 'touchstart', 'touchmove'],
+			onLeave: (event, chartElement, chart) => {
 				chart.data.datasets.forEach((dataset) => {
 					dataset.borderWidth = 4;
 				});
 			},
 			onHover: (event, chartElement, chart) => {
-				if (chartElement.length > 0) {
-					const firstPoint = chartElement[0];
-					// Check if the hovered element is a line
-					if (firstPoint && firstPoint.datasetIndex !== undefined) {
-						const elements = chart.getElementsAtEventForMode(
-							event,
-							'nearest',
-							{ intersect: false },
-							true
-						);
-						// If there are elements (i.e., the line), apply custom animation or effects
-						if (elements.length > 0) {
-							console.log('Hovering over the line!');
+				// If there are elements (i.e., the line), apply custom animation or effects
+				console.log('Hovering over the line!');
 
-							chart.data.datasets.forEach((dataset, i) => {
-								if (elements[0].datasetIndex === i) {
-									dataset.borderWidth = 15; // Highlight the hovered dataset
-								} else {
-									dataset.borderWidth = 5; // Reset other datasets
-								}
-							});
-						}
+				chart.data.datasets.forEach((dataset, i) => {
+					if (chartElement[0].datasetIndex === i) {
+						dataset.borderWidth = 8; // Highlight the hovered dataset
+						dataset.pointRadius = 8; // Highlight the hovered dataset
+					} else {
+						dataset.borderWidth = 4; // Reset other datasets
+						dataset.pointRadius = 5; // Highlight the hovered dataset
 					}
-				} else {
-					// If not hovering over any dataset, reset all datasets
-					chart.data.datasets.forEach((dataset) => {
-						dataset.borderWidth = 4;
-					});
-				}
+				});
 				chart.update({
 					duration: 150,
 					easing: 'easeOutBounce'
---
+++ frontend/src/lib/RankEChart.svelte
@@ -0,0 +1,132 @@
+<script>
+	import { onMount } from 'svelte';
+
+	// const theme = skeleton.getThemeProperties('skeleton');
+	// const bgColor = theme['--color-primary-500'];
+
+	import * as echarts from 'echarts';
+
+	let myChartDiv, myChart;
+
+	const myDraculaColors = ['#ff79c6', '#bd93f9', '#1ef956', '#f67516'];
+
+	const years = ['2001', '2002', '2003', '2004', '2005', '2006'];
+
+	let chartoption = {
+		color: myDraculaColors,
+		tooltip: {
+			trigger: 'item'
+		},
+		// grid: {
+		// 	left: 30,
+		// 	right: 110,
+		// 	bottom: 30,
+		// 	containLabel: true
+		// },
+		xAxis: {
+			type: 'category',
+			splitLine: {
+				show: true
+			},
+			// axisLabel: {
+			// 	margin: 30,
+			// 	fontSize: 16
+			// },
+			// boundaryGap: false,
+			data: years
+		},
+		yAxis: {
+			type: 'value',
+			// axisLabel: {
+			// 	margin: 34,
+			// 	fontSize: 28,
+			// 	formatter: '#{value}'
+			// },
+			inverse: true,
+			interval: 1,
+			min: 1,
+			max: 8
+		},
+		series: [
+			{
+				name: '# of Votes',
+				data: [1, 1, 2, 3, 6, 8],
+				type: 'line',
+				symbolSize: 20,
+				smooth: true,
+				emphasis: {
+					focus: 'series'
+				},
+				endLabel: {
+					show: true,
+					formatter: '{a}',
+					distance: 18,
+					fontSize: 20
+				},
+				lineStyle: {
+					width: 4
+				}
+			},
+			{
+				name: '# of Points',
+				type: 'line',
+				data: [2, 4, 4, 5, 3, 5],
+				symbolSize: 20,
+				smooth: true,
+				emphasis: {
+					focus: 'series'
+				},
+				endLabel: {
+					show: true,
+					formatter: '{a}',
+					distance: 18,
+					fontSize: 20
+				},
+				lineStyle: {
+					width: 4
+				}
+			},
+			{
+				name: '# of Cars',
+				type: 'line',
+				data: [5, 6, 7, 6, 5, 4],
+				symbolSize: 20,
+				smooth: true,
+				emphasis: {
+					focus: 'series'
+				},
+				endLabel: {
+					show: true,
+					formatter: '{a}',
+					distance: 20
+				},
+				lineStyle: {
+					width: 4
+				}
+			}
+		]
+	};
+
+	onMount(() => {
+		// Create the echarts instance
+		myChart = echarts.init(myChartDiv);
+		// Draw the chart
+		myChart.setOption(chartoption);
+		myChart.resize();
+	});
+
+	$: if (myChart) {
+		// Create the echarts instance
+		myChart.dispose();
+		myChart = echarts.init(myChartDiv);
+		// Draw the chart
+		myChart.setOption(chartoption);
+		myChart.resize();
+	}
+</script>
+
+<!-- <canvas bind:this={canvas} /> -->
+
+<!-- <svelte:window innerWidth={p} innerHeight={q} /> -->
+<!-- <div id="echartsRank" style="width: 600px;height:400px;" /> -->
+<div class="w-full h-96" bind:this={myChartDiv} />
