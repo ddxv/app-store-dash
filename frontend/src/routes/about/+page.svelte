5e33b83e407117ea0e182fc2a8fd466097fc249b
james <james@bubbleye.com>
2023-11-03 16:07:17 +0800

    Better about

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -5,31 +5,50 @@
 </script>
 
 <h1 class="h1">Thanks for checking out this project</h1>
-This is something I've been wasting my time on for quite awhile. I started this as something of a curiousity,
-then used this data for examining App Ads.txt files. All code for this data scraping, database management
-and different frontend dashboards is available on GitHub. If you have any questions, please feel free
-to reach out chat.
-
-<TabGroup
-	justify="justify-end"
-	active="variant-filled-primary"
-	hover="hover:variant-soft-primary"
-	flex="flex-1 lg:flex-none"
-	rounded=""
-	border=""
-	class="w-full hidden md:inline-flex"
->
-	<TabAnchor href="https://ads.jamesoclaire.com/dash/ads">
-		<span>Ads.txt Dash</span>
-	</TabAnchor>
-	<TabAnchor href="https://jamesoclaire.com">
-		<span>Blog</span>
-	</TabAnchor>
-</TabGroup>
 <br />
+<strong> Background: </strong>
+<p>
+	Originally this came out of a curiosity I had about the IAB app-ads.txt standard in 2022. In order
+	to scrape app-ads.txt files I had to first get all the iOS and Android apps + their respective
+	publisher URLs. While that project, like app-ads.txt itself, went unused, I remained interested in
+	letting the database of apps keep growing. Then in the fall of 2023 while on haitus from real
+	work, I started working on an API and UI.
+</p>
+<br />
+<p>
+	Now, App Goblin is the combination of several open source projects I built. Yes, there are a large
+	number of similar companies like data.ai and sensortower.com which have many more features. But it
+	was a great project for me to learn more frontend and javascript as well as give me something to
+	do with the scraper which I had built previously (whats the point of a database if you cant use it
+	somehow). For now App Goblin is simply here as a resource free of charge as it's running costs
+	only $20 a month or so in hosting.
+</p>
+<br />
+<p>If you have questions or suggestions please feel free to reach out.</p>
+<a href="https://jamesoclaire.com">
+	<h2>
+		<span>jamesoclaire.com</span>
+	</h2>
+</a>
 <br />
 
-<h2 class="h2">Github Readme</h2>
-{#await fetch(url).then((r) => r.text()) then text}
-	{@html marked(text)}
-{/await}
+<p>
+	<strong> Open Source Tech Stack: </strong>
+</p>
+<p>
+	First is the underlying data which is collected and maintained from <strong
+		><a href="https://github.com/ddxv/adscrawler">github.com/ddxv/adscrawler</a></strong
+	>
+	The adscrawler uses Python & PostgreSQL to crawl app stores to collect apps and their basic information
+	like names and rating counts. This data is then stored to the PostgreSQL database.
+</p>
+<br />
+<p>
+	The website itself is also open source and can be found at
+	<strong
+		><a href="https://github.com/ddxv/app-store-dash">github.com/ddxv/app-store-dash</a></strong
+	> The website is built from a Python backend API based on LiteStar which manages queries to the PostgreSQL
+	database. The API is used by a JavaScript Svelte frontend with Tailwind and Skeleton for CSS. All projects
+	are glued together with systemd and websockets where needed. Everything is hosted on AWS in the small
+	EC2 instances.
+</p>
