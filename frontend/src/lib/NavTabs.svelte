036c1a3f3d5e7c83b9856667ef079d3470d26d19
james <james@bubbleye.com>
2023-11-03 14:50:26 +0800

    Add Nav bar for small device'

---
+++ frontend/src/lib/NavTabs.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { TabGroup, TabAnchor } from '@skeletonlabs/skeleton';
+</script>
+
+<TabGroup
+	active="variant-filled-primary"
+	hover="hover:variant-soft-primary"
+	flex="flex-1"
+	rounded=""
+	border=""
+	class="w-full p-0"
+>
+	<TabAnchor class="p-0 px-0" href="/" selected={$page.url.pathname.startsWith('/collections')}>
+		<h2 class="h5 md:h2 md:p-2">HOME</h2>
+	</TabAnchor>
+	<TabAnchor
+		href="/rankings/store/1/collection/1/category/1"
+		selected={$page.url.pathname.startsWith('/rankings')}
+	>
+		<h2 class="h5 md:h2 md:p-2">RANKS</h2>
+	</TabAnchor>
+	<!-- <TabAnchor href="/categories" selected={$page.url.pathname === '/categories'}
+					>CATEGORIES
+				</TabAnchor> -->
+	<TabAnchor href="/about" selected={$page.url.pathname === '/about'}
+		><h2 class="h5 md:h2 md:p-2">ABOUT</h2></TabAnchor
+	>
+</TabGroup>
