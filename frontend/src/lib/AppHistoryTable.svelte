56eecb299b489d39821de6fce4d3692d3e8b81ef
james <james@bubbleye.com>
2023-11-07 13:50:59 +0800

    Fix table for mobile only

--- frontend/src/lib/AppHistoryTable.svelte
+++ frontend/src/lib/AppHistoryTable.svelte
@@ -12,41 +12,41 @@
 
 <div class="card variant-glass-surface mt-2 md:mt-4 md:p-4">
 	<h4 class="h4 md:h3 p-2 mt-2">Recent Raw Data</h4>
-	<div class="table-container overflow-x-auto">
-		<table class="table table-hover table-auto">
+	<div class="overflow-x-auto pl-0">
+		<table class="table md:table-hover table-compact pl-0">
 			<thead>
 				<tr>
-					<th>Date</th>
+					<th class="table-cell-fit !pl-1 !pr-0">Date</th>
 					{#if os == 'google'}
-						<th>Installs</th>
+						<th class="table-cell-fit !px-0">Installs</th>
 					{/if}
-					<th>Rating</th>
-					<th>Ratings</th>
-					<th>Reviews</th>
+					<th class="table-cell-fit !px-0">Rating</th>
+					<th class="table-cell-fit !px-0">Ratings</th>
+					<th class="table-cell-fit !px-0">Reviews</th>
 				</tr>
 			</thead>
 			<tbody>
 				{#each Object.entries(history_table) as [_prop, values]}
 					<tr>
-						<td>
+						<td class="!pl-1 !pr-0">
 							{values.crawled_date}
 						</td>
 						{#if os == 'google'}
-							<td>
+							<td class="table-cell-fit !px-0">
 								{values.installs}
 							</td>
 						{/if}
-						<td>
+						<td class="table-cell-fit !px-0">
 							{#if values.rating}
 								{values.rating.toFixed(2)}
 							{:else}
 								null
 							{/if}
 						</td>
-						<td>
+						<td class="table-cell-fit !px-0">
 							{values.rating_count}
 						</td>
-						<td>
+						<td class="table-cell-fit !px-0">
 							{values.review_count}
 						</td>
 					</tr>
