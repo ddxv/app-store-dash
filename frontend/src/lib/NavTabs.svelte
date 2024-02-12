98a4ee27b6585e7aa205480a192fbd2b67026abb
james <james@bubbleye.com>
2024-02-12 13:29:39 +0800

    Change padding to try fitting more on bottom menu

--- frontend/src/lib/NavTabs.svelte
+++ frontend/src/lib/NavTabs.svelte
@@ -10,30 +10,30 @@
 	rounded=""
 	border=""
 	class="w-full p-0"
+	padding="px-1 py-4 md:px-4"
 >
 	<TabAnchor class="p-0 px-0" href="/" selected={$page.url.pathname == '/'}>
-		<h3 class="h4 md:h3 md:p-2">HOME</h3>
+		<h3 class="h5 md:h3">HOME</h3>
 	</TabAnchor>
 	<TabAnchor
-		class="p-0 px-0"
 		href="/collections/new_monthly"
 		selected={$page.url.pathname.startsWith('/collections')}
 	>
-		<h3 class="h4 md:h3 md:p-2">NEW</h3>
+		<h3 class="h5 md:h3">NEW</h3>
 	</TabAnchor>
 	<TabAnchor
 		href="/rankings/store/1/collection/1/category/1"
 		selected={$page.url.pathname.startsWith('/rankings')}
 	>
-		<h3 class="h4 md:h3 md:p-2">RANKS</h3>
+		<h3 class="h5 md:h3">RANKS</h3>
 	</TabAnchor>
 	<TabAnchor href="/trackers" selected={$page.url.pathname === '/trackers'}
-		><h3 class="h4 md:h3 md:p-2">TRACKERS</h3></TabAnchor
+		><h3 class="h5 md:h3">TRACKERS</h3></TabAnchor
 	>
 	<TabAnchor href="/networks" selected={$page.url.pathname === '/networks'}
-		><h3 class="h4 md:h3 md:p-2">NETWORKS</h3></TabAnchor
+		><h3 class="h5 md:h3">NETWORKS</h3></TabAnchor
 	>
 	<TabAnchor href="/about" selected={$page.url.pathname === '/about'}
-		><h3 class="h4 md:h3 md:p-2">ABOUT</h3></TabAnchor
+		><h3 class="h5 md:h3">ABOUT</h3></TabAnchor
 	>
 </TabGroup>
