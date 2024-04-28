7ad781f71092743e9669bb022cdbaa1fa6da85c7
james <jamesoclaire@gmail.com>
2024-04-28 10:05:35 +0800

    Added Apple icon to sidebar for companies

--- frontend/src/lib/SideBar.svelte
+++ frontend/src/lib/SideBar.svelte
@@ -318,13 +318,22 @@
 									<div class="flex-grow">
 										{values.name}
 									</div>
-									{#if Number(values.android) > 0}
+									{#if Number(values.android) > 0 || values.name == 'Games'}
 										<div class="justify-end mr-2 md:mr-5">
 											<IconGoogle size="10" />
 										</div>
 									{:else}
 										<div class="opacity-20 justify-end mr-2 md:mr-5">
 											<IconGoogle size="10" />
+										</div>
+									{/if}
+									{#if Number(values.ios) > 0}
+										<div class="justify-end mr-2 md:mr-5">
+											<IconiOS size="10" />
+										</div>
+									{:else}
+										<div class="opacity-20 justify-end mr-2 md:mr-5">
+											<IconiOS size="10" />
 										</div>
 									{/if}
 								</div>
@@ -377,18 +386,23 @@
 									<div class="flex-grow">
 										{values.name}
 									</div>
+
 									{#if Number(values.android) > 0 || values.name == 'Games'}
 										<div class="justify-end mr-2 md:mr-5">
+											<IconGoogle size="10" />
+										</div>
+									{:else}
+										<div class="opacity-20 justify-end mr-2 md:mr-5">
 											<IconGoogle size="10" />
 										</div>
 									{/if}
 									{#if Number(values.ios) > 0}
 										<div class="justify-end mr-2 md:mr-5">
-											<IconiOs size="10" />
+											<IconiOS size="10" />
 										</div>
 									{:else}
 										<div class="opacity-20 justify-end mr-2 md:mr-5">
-											<IconGoogle size="10" />
+											<IconiOS size="10" />
 										</div>
 									{/if}
 								</div>
