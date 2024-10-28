15e098ced3d7402b0c09d9ef68d7d73a055d4dd9
James O'Claire <jamesoclaire@gmail.com>
2024-10-28 22:11:02 +0800

    Add search results for companies as well as apps

---
+++ backend/dbcon/sql/query_search_companies.sql
@@ -0,0 +1,26 @@
+-- noqa: disable=LT02
+-- noqa: disable=LT08
+-- noqa: disable=RF02
+-- noqa: disable=PRS
+-- SQLFluff currently unable to parse the double @ below
+-- https://github.com/sqlfluff/sqlfluff/issues/4837
+SELECT
+    cac.tag_source,
+    cac.store,
+    cac.company_domain,
+    cac.company_name,
+    sum(app_count) AS app_count
+FROM
+    adtech.companies_categories_types_app_counts cac
+WHERE
+    cac.company_name ILIKE '%' || :searchinput || '%'
+    OR cac.company_domain ILIKE '%' || :searchinput || '%'
+GROUP BY
+    cac.tag_source,
+    cac.store,
+    cac.company_domain,
+    cac.company_name
+ORDER BY
+    app_count DESC
+LIMIT :mylimit;
+
