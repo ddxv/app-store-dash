5f4a1245131a925e0b778f2841e179250425c5e7
James O'Claire <jamesoclaire@gmail.com>
2024-11-21 10:16:43 +0800

    Add header for developer page

--- frontend/src/routes/developers/[developer]/+page.svelte
+++ frontend/src/routes/developers/[developer]/+page.svelte
@@ -1,12 +1,55 @@
 <script lang="ts">
 	import type { DeveloperResponse } from '../../../types';
 	import AppsCard from '$lib/AppGroupCard.svelte';
+	import { page } from '$app/stores';
 	interface Props {
 		data: DeveloperResponse;
 	}
 
 	let { data }: Props = $props();
 </script>
+
+<svelte:head>
+	<link rel="canonical" href="https://appgoblin.info/developers/{$page.params.developer}" />
+	{#await data.results then dev}
+		<title>{dev.title} Android Trends | {$page.params.developer} | AppGoblin Developer Data</title>
+		<meta
+			name="description"
+			content="Explore {dev.title} Android & iOS app's analytics and market trends with AppGoblin. Detailed app rankings and download statistics to inform your Android & iOS app strategy and discover top-performing apps."
+		/>
+		<meta
+			name="keywords"
+			content="{dev.title}, {$page.params
+				.developer}, analytics, ads, market data, Android app rankings, app reviews, download statistics, Google Play data, app comparison, mobile app insights, Android"
+		/>
+		<meta
+			property="og:title"
+			content="{dev.title} Android App Stats & Info - AppGoblin | {$page.params.developer}"
+		/>
+		<meta
+			property="og:description"
+			content="Explore {dev.title} Android & iOS app analytics and market trends with AppGoblin. {dev.title} by {$page
+				.params
+				.developer}. Dive into detailed app rankings and download statistics to inform your Android & iOS app strategy and discover top-performing apps."
+		/>
+		<meta
+			name="twitter:title"
+			content="{dev.title} Android & iOS | {$page.params.developer} | App Stats & Info - AppGoblin"
+		/>
+		<meta
+			name="twitter:description"
+			content="Explore {dev.title} Android & iOS app analytics and market trends with AppGoblin. {dev.title} by {$page
+				.params
+				.developer}. Dive into detailed app rankings and download statistics to inform your Android & iOS app strategy and discover top-performing apps."
+		/>
+
+		<meta property="og:image" content="https://appgoblin.info/goblin_purple_hat_250.png" />
+		<meta property="og:url" content="https://appgoblin.info/" />
+		<meta property="og:type" content="website" />
+		<meta name="twitter:card" content="summary_large_image" />
+		<meta name="twitter:image" content="https://appgoblin.info/goblin_purple_hat_250.png" />
+	{/await}
+</svelte:head>
 
 <div>
 	{#await data.results}
