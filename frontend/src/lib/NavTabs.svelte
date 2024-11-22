be26d400b95f42f24ba8e311f0178396e2b0acac
James O'Claire <jamesoclaire@gmail.com>
2024-11-22 11:06:13 +0800

    rearrange

--- frontend/src/lib/NavTabs.svelte
+++ frontend/src/lib/NavTabs.svelte
@@ -11,9 +11,10 @@
 </script>
 
 <div class="flex flex-row gap-2 md:gap-6 text-center items-center {topDivider} mx-2">
-	<a href="/collections/new_monthly">
-		<p class={isHighlighted('/collections') ? topBarHighlightedFont : topBarFont}>NEW APPS</p>
+	<a href="/companies" class={topBarFont}>
+		<p class={isHighlighted('/companies') ? topBarHighlightedFont : topBarFont}>COMPANY RANKS</p>
 	</a>
+
 	<div class={horizontalDivider}></div>
 	<a href="/rankings/store/1/collection/1/category/1" class={topBarFont}>
 		<p class={isHighlighted('/rankings') ? topBarHighlightedFont : topBarFont}>APP RANKS</p>
@@ -22,9 +23,10 @@
 	<a href="/sdks" class={topBarFont}>
 		<p class={isHighlighted('/sdks') ? topBarHighlightedFont : topBarFont}>APP SDK</p>
 	</a>
+
 	<div class={horizontalDivider}></div>
-	<a href="/companies" class={topBarFont}>
-		<p class={isHighlighted('/companies') ? topBarHighlightedFont : topBarFont}>COMPANY RANKS</p>
+	<a href="/collections/new_monthly">
+		<p class={isHighlighted('/collections') ? topBarHighlightedFont : topBarFont}>NEW APPS</p>
 	</a>
 	<div class={horizontalDivider}></div>
 	<a href="/about" class={topBarFont}>
