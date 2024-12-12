802d032ed42e98322b6fdf2ed511760ebd36ad0c
James O'Claire <jamesoclaire@gmail.com>
2024-12-12 15:15:50 +0800

    Removed unused code

--- frontend/src/routes/(categorical)/companies/(overview)/categories/[category]/+page.svelte
+++ frontend/src/routes/(categorical)/companies/(overview)/categories/[category]/+page.svelte
@@ -1,65 +1,15 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import CompaniesBarChart from '$lib/CompaniesBarChart.svelte';
 	import CompaniesOverviewTable from '$lib/CompaniesOverviewTable.svelte';
-	import TotalsBox from '$lib/TotalsBox.svelte';
-	import WhiteCard from '$lib/WhiteCard.svelte';
-	import CompaniesLayout from '$lib/CompaniesLayout.svelte';
 	import CompaniesTableGrid from '$lib/CompaniesTableGrid.svelte';
 
 	import type { PageData } from './$types';
 
 	let { data }: { data: PageData } = $props();
 
-	let currentCategoryName = $derived(getCategoryName($page.params.category));
-
-	function getCategoryName(category: string) {
-		return (
-			data?.appCats?.categories?.find((cat: { id: string }) => cat.id == category)?.name || category
-		);
-	}
-
 	function formatNumber(num: number) {
 		return new Intl.NumberFormat('en-US').format(num);
 	}
 </script>
-
-{#await data.companiesOverview}
-	<div class="card preset-tonal p-6 rounded-lg shadow-md flex justify-center items-center h-40">
-		<span class="text-lg">Loading...</span>
-	</div>
-{:then myData}
-	{#if typeof myData == 'string'}
-		<p class="text-red-500 text-center">Failed to load company details.</p>
-	{:else if myData.categories.categories.all}
-		<CompaniesLayout>
-			{#snippet card1()}
-				<WhiteCard>
-					<TotalsBox myTotals={myData.categories.categories.all} myType={currentCategoryName} />
-				</WhiteCard>
-			{/snippet}
-
-			{#snippet card2()}
-				<WhiteCard>
-					{#snippet title()}
-						Top SDK Companies
-					{/snippet}
-					<CompaniesBarChart plotData={myData.top.sdk} />
-				</WhiteCard>
-			{/snippet}
-			{#snippet card3()}
-				<WhiteCard>
-					{#snippet title()}
-						Top Adstxt Companies
-					{/snippet}
-					<CompaniesBarChart plotData={myData.top.adstxt_direct} />
-				</WhiteCard>
-			{/snippet}
-		</CompaniesLayout>
-	{/if}
-{:catch error}
-	<p class="text-red-500 text-center">{error.message}</p>
-{/await}
 
 {#await data.companiesOverview}
 	<div><span>Loading...</span></div>
